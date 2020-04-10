6d70b5ec64bcc8e18ad0821ecc6ac9e4c582fb42
Noor Muhammad <connectwithnoor1@gmail.com>
2020-04-10 18:39:58 +0500

    setup: mongoose models

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
+
 require('dotenv').config();
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World!!!',
+    getPosts: async () => {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
   },
 };
 
