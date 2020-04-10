7829df6b7e7b366fe5491e77e6f4b395ffa34f0f
Noor Muhammad <connectwithnoor1@gmail.com>
2020-04-10 22:51:22 +0500

    added: likeCount and commentCount

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,10 @@ const UsersResolvers = require('./users');
 const CommentsResolvers = require('./comments');
 
 module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
     ...PostsResolvers.Query,
   },
