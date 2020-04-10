155a02f593c167432ecdadc51c0fd10387143a20
Noor Muhammad <connectwithnoor1@gmail.com>
2020-04-10 20:18:58 +0500

    setup: getPost query

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -2,6 +2,7 @@ const Post = require('../../models/Post');
 
 module.exports = {
   Query: {
+    // GET POSTS
     getPosts: async () => {
       try {
         const posts = await Post.find();
@@ -10,5 +11,20 @@ module.exports = {
         throw new Error(error);
       }
     },
+
+    // CREATE POST
+    getPost: async (_, { postId }) => {
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          return post;
+        } else {
+          throw new Error('Post not found!');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+    // DELETE POST
   },
 };
