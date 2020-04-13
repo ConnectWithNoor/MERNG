0af37d55babebebb115a9d1881ee6b668d91b100
Noor Muhammad <connectwithnoor1@gmail.com>
2020-04-13 13:34:04 +0500

    improved: client side for production

--- merng-client/src/ApolloProvider.js
+++ merng-client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { setContext } from 'apollo-link-context';
 
 import App from './App';
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000',
+  uri: 'https://agile-reaches-44040.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
