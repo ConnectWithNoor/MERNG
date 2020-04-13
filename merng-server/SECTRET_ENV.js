b908146bb40a09120217d539e19312c5c710613e
Noor Muhammad <connectwithnoor1@gmail.com>
2020-04-13 13:03:28 +0500

    test

---
+++ merng-server/SECTRET_ENV.js
@@ -0,0 +1,8 @@
+const MONGODB =
+  'mongodb+srv://noor:R0cS9xzyOm9G4G9a@cluster0-oleac.mongodb.net/merng?retryWrites=true&w=majority';
+const SECRET_KEY = '93u2j2@k03';
+
+module.exports = {
+  MONGODB,
+  SECRET_KEY,
+};
